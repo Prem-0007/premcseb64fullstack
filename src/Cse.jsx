4ea50924f74405cabdfaf94184d19c1295844d03
Prem-0007 <ballapremkumar777@gmail.com>
2026-02-08 14:44:29 +0530

    added demo files

---
+++ src/Cse.jsx
@@ -0,0 +1,10 @@
+function Cse () {
+    
+    return (
+        <div>
+            <h1>Hello from Cse.jsx!</h1>
+            <p>This is the CSE department page.</p>
+        </div>
+    )
+}
+export default  Cse 
